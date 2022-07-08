6593fc42b207cefc941d08503f8b0f5c5de5579b
unknown <1975tanya@bk.ru>
2022-07-08 11:54:19 +0300

    add 1_3

---
+++ First_Lesson/1_3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Write a number: ");
+int a=int.Parse(Console.ReadLine());
+if (a==1){
+    Console.WriteLine("понедельник");
+}
+else if (a==2)
+{
+  Console.WriteLine("вторник");  
+}
+else if (a==3)
+{
+  Console.WriteLine("среда");  
+}
+else if (a==4)
+{
+  Console.WriteLine("четверг");  
+}
+else if (a==5)
+{
+  Console.WriteLine("пятница");  
+}
+else if (a==6)
+{
+  Console.WriteLine("суббота");  
+}
+else if (a==7)
+{
+  Console.WriteLine("воскресенье");  
+}
+else {
+    Console.WriteLine("Ошибка ввода");   
+}
+
+
