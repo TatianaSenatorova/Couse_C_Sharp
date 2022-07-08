a5224cd9ef65130b705a4c83a63d1840ffefb99f
unknown <1975tanya@bk.ru>
2022-07-08 11:34:34 +0300

    add example 2

---
+++ First_Lesson/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Write a number a ");
+string s_a=(Console.ReadLine());
+int a=int.Parse(s_a);
+Console.WriteLine("Write a number b ");
+string s_b=(Console.ReadLine());
+int b=int.Parse(s_b);
+if(a==b*b)
+{
+    Console.WriteLine("верно");
+}
+else{
+   Console.WriteLine("неверно"); 
+}
