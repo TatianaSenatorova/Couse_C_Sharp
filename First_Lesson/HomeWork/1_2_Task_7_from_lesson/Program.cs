fa12d96f69106a0758358bcbe36a34a361a681c8
unknown <1975tanya@bk.ru>
2022-07-10 20:06:08 +0300

    add task 7 from lesson

---
+++ First_Lesson/HomeWork/1_2_Task_7_from_lesson/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа.*/
+
+Console.WriteLine("Write a tree-digit number a: ");
+Console.WriteLine(int.Parse(Console.ReadLine())%10);
