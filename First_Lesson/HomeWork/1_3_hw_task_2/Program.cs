818fa7e9d57e5785e85e56609e1e79f119d9ef60
unknown <1975tanya@bk.ru>
2022-07-10 20:11:35 +0300

    add task 2 homework

---
+++ First_Lesson/HomeWork/1_3_hw_task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.*/
+
+Console.WriteLine("Write a first number a: ");
+Console.WriteLine("Write a second number b: ");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+if(a>b){
+    Console.WriteLine(a);
+}
+else{
+    Console.WriteLine(b);
+}
+
+
