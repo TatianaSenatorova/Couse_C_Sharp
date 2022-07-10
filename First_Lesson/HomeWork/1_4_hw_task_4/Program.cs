1e3ed7d19104a44cb1d668c9304c8c8a6edd3987
unknown <1975tanya@bk.ru>
2022-07-10 20:17:14 +0300

    add task 4 homework

---
+++ First_Lesson/HomeWork/1_4_hw_task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Write a first number a;");
+Console.WriteLine("Write a second number b;");
+Console.WriteLine("Write a third number c;");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int c=int.Parse(Console.ReadLine());
+int max=a;
+if(b>max){
+    max=b;
+}
+if(c>max){
+    max=c;
+}
+Console.WriteLine(max);
+
+
