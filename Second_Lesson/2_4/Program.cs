3f25ccd125ff3a16ea2315759289d475adad846e
unknown <1975tanya@bk.ru>
2022-07-13 12:28:11 +0300

    add task 4

---
+++ Second_Lesson/2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход
+число и проверяет, кратно ли оно одновременно 7 и
+23.*/
+
+void Multiple (int num){
+    if (num%7==0 && num%23==0) 
+         Console.WriteLine("Кратно"); 
+    else Console.WriteLine("Некратно");
+}
+
+Console.WriteLine("Write a number: ");
+int a=int.Parse(Console.ReadLine());
+Multiple(a);
