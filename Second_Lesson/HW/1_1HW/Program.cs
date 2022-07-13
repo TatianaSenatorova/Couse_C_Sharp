1233e7c8b45b7868d493ea1399cedc3265e74abd
unknown <1975tanya@bk.ru>
2022-07-13 17:16:45 +0300

    add task 1 homework

---
+++ Second_Lesson/HW/1_1HW/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+/*int SecondDigit(int Number){
+              return ((Number/10)%10); 
+        }
+Console.WriteLine("Write a three-digit number: ");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine(SecondDigit(a));*/
+
+
+void SecondDigit(int Number){
+        Double b=Number/100;
+        if(1<=b && b<10)
+            Console.WriteLine($"The second digit: {(Number/10)%10}"); 
+        else
+            Console.WriteLine("You had written a wrong number (not a three-digit)");
+   }
+Console.WriteLine("Write a three-digit number: ");
+int a=int.Parse(Console.ReadLine());
+SecondDigit(a);
