37ae2914f98945e4426339bbcc433160f7c09b3e
unknown <1975tanya@bk.ru>
2022-07-13 18:38:22 +0300

    add task 2

---
+++ Second_Lesson/HW/1_2HW/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+int a=new Random().Next(100, 1000);
+Console.WriteLine($"Three-digit random number {a}");
+Console.WriteLine($"This number without second digit {a/100}{a%10}");
