e81ccaa220d83dd31a200700aa119698585c153c
unknown <1975tanya@bk.ru>
2022-07-15 18:42:37 +0300

    add task 3_1_1

---
+++ 3_Lesson/3_1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y)*/
+
+void Coodinates(int a)
+{
+    if(a==1)
+        Console.WriteLine("x>0 and y>0");
+    else if(a==2)
+        Console.WriteLine("x<0 and y>0");
+    else if(a==3)
+        Console.WriteLine("x<0 and y<0");
+    else if(a==4)
+        Console.WriteLine("x>0 and y<0");
+        else Console.WriteLine("You are wrong");
+}
+Coodinates(1);
+Coodinates(2);
+Coodinates(3);
+Coodinates(4);
+Coodinates(10);
