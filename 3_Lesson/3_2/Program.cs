63c28f4ee778a68c5de9bdd0c9c3bd32ec4b0f9e
unknown <1975tanya@bk.ru>
2022-07-15 19:15:18 +0300

    add task 3_2

---
+++ 3_Lesson/3_2/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и
+находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21*/
+
+double Distance(double Ax, double Ay, double Bx, double By)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(Bx-Ax,2)+Math.Pow(By-Ay,2)),4);
+}
+Console.WriteLine(Distance(3, 6, 2, 1));
