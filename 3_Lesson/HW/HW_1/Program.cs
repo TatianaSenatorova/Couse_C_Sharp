ff38abae8ac9602a189bb9e4b15fd44a5ac3d428
unknown <1975tanya@bk.ru>
2022-07-16 21:04:51 +0300

    add task 1 homework

---
+++ 3_Lesson/HW/HW_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+/*First version*/
+void Palindrome(int a)
+{
+    int b=a/1000;
+    int c=a/10;
+    if(a/10000==a%10 && b%10==c%10)
+        Console.WriteLine($"{a} Yes, it`s a palindrome");
+    else  Console.WriteLine($"{a} No, it`s not a palindrome");
+}
+Palindrome(14212);
+Palindrome(12821);
+Palindrome(23432);
+
+/*Second version*/
+
+/*void Palindrome(string a)
+{
+    if(a.Substring(0, 1)==a.Substring(4, 1) && a.Substring(1, 1)==a.Substring(3, 1))
+    Console.WriteLine($"{a} Yes, it`s a palindrome");
+    else  Console.WriteLine($"{a} No, it`s not a palindrome");
+}
+Palindrome("14212");
+Palindrome("12821");
+Palindrome("23432");*/
