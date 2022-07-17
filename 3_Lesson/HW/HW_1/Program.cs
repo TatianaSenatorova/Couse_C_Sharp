656e5f192dc7aa7124ae1519feb30807cbbd125b
unknown <1975tanya@bk.ru>
2022-07-17 11:41:44 +0300

    add one more version to task 1

--- 3_Lesson/HW/HW_1/Program.cs
+++ 3_Lesson/HW/HW_1/Program.cs
@@ -7,6 +7,45 @@
 /*First version*/
 void Palindrome(int a)
 {
+    string b=a.ToString();
+    int N=b.Length;
+    int i=0;
+    while (i<N/2)
+        {
+            if(b.Substring(i, 1)==b.Substring(N-1-i, 1))
+            {
+                i++;
+                if (i==(N-1)/2)  
+                {Console.WriteLine($"{a} Yes, it`s a palindrome");}
+            }
+            else  
+            {
+                Console.WriteLine($"{a} No, it`s not a palindrome");
+                break;
+            }
+         }
+            
+}
+Console.WriteLine("Write any number: ");
+int a=int.Parse(Console.ReadLine());
+Palindrome(a);
+
+/*Second version*/
+/*void Palindrome(int a)
+{
+    string b=a.ToString();
+    if(b.Substring(0, 1)==b.Substring(4, 1) && b.Substring(1, 1)==b.Substring(3, 1))
+    Console.WriteLine($"{b} Yes, it`s a palindrome");
+    else  Console.WriteLine($"{b} No, it`s not a palindrome");
+}
+Palindrome(14212);
+Palindrome(12821);
+Palindrome(23432);*/
+
+/*Third version*/
+
+/*void Palindrome(int a)
+{
     int b=a/1000;
     int c=a/10;
     if(a/10000==a%10 && b%10==c%10)
@@ -15,16 +54,5 @@ void Palindrome(int a)
 }
 Palindrome(14212);
 Palindrome(12821);
-Palindrome(23432);
+Palindrome(23432);*/
 
-/*Second version*/
-
-/*void Palindrome(string a)
-{
-    if(a.Substring(0, 1)==a.Substring(4, 1) && a.Substring(1, 1)==a.Substring(3, 1))
-    Console.WriteLine($"{a} Yes, it`s a palindrome");
-    else  Console.WriteLine($"{a} No, it`s not a palindrome");
-}
-Palindrome("14212");
-Palindrome("12821");
-Palindrome("23432");*/
