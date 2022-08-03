ed358655274ed3d4b79defe0a3505ebb7e4fd7d8
unknown <1975tanya@bk.ru>
2022-08-03 20:54:50 +0300

    Task 4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 
+*/
+
+/*First variant*/
+/*void Num(int A)
+{
+    string number=A.ToString();
+    Console.WriteLine(number.Length);
+}
+Num(123);*/
+
+/*Second variant*/
+
+int NumCount(int num)
+{
+    int result=0;
+    while (num>0)
+    {
+        num/=10;
+        result+=1;
+    }
+    return result;
+}
+Console.WriteLine(NumCount(456));
+Console.WriteLine(NumCount(78));
+Console.WriteLine(NumCount(89126));
