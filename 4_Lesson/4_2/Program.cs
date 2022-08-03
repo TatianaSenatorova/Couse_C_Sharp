d3c59115fc96426bec42e16e3ca5fb211beadc84
unknown <1975tanya@bk.ru>
2022-08-03 23:13:32 +0300

    Task 4_2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int multiply(int N)
+{
+    int num=1;
+    for (int i=2; i<=N; i++)
+    num*=i;
+    return num;
+}
+Console.WriteLine(multiply(4));
+Console.WriteLine(multiply(5));
