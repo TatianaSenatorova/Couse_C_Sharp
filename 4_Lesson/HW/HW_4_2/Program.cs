556e31586ed0fd5855c39d96613df9f4dac14ebb
unknown <1975tanya@bk.ru>
2022-08-04 14:35:11 +0300

    Task HW_4_2

---
+++ 4_Lesson/HW/HW_4_2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая принимает на
+вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+/*First variant mathematical*/
+
+/*int SumNum(int A)
+{
+    int result=0;
+    while (A>0)
+    {
+    result=result+A%10;
+    A/=10;
+    }
+    return result;
+}
+Console.WriteLine(SumNum(452));
+Console.WriteLine(SumNum(82));
+Console.WriteLine(SumNum(9012));*/
+
+/*Second variant array*/
+
+int SumNum(int A)
+{
+   int [] array=new int [A];
+   int Sum=0;
+   for (int i=0; A>0; i++)
+   {
+       array[i]=A%10;
+       A/=10;
+       Sum=Sum+array[i];
+   }
+return Sum;
+}
+Console.WriteLine(SumNum(452));
+Console.WriteLine(SumNum(82));
+Console.WriteLine(SumNum(9012));
