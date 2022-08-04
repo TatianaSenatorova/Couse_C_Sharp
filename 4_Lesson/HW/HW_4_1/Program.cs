8fa3b7c4bbe66c52e989c12d6d1cd26e19c1328e
unknown <1975tanya@bk.ru>
2022-08-04 11:10:41 +0300

    Task HW_4_1

---
+++ 4_Lesson/HW/HW_4_1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+/*First variant with cycle*/
+
+int Exponent(int A, int B)
+{
+    int result=1;
+    for (int i=1; i<=B; i++)
+    result=result*A;
+    return result;
+  }
+Console.WriteLine(Exponent(3, 5));
+Console.WriteLine(Exponent(2, 4));
+
+/*Second variant without cycle*/
+
+/*void Exponent(int A, int B)
+{
+    Console.WriteLine($"{A} в степени {B} →  {Math.Pow(A,B)}");
+  }
+Exponent(3, 5);
+Exponent(2, 4);*/
+
